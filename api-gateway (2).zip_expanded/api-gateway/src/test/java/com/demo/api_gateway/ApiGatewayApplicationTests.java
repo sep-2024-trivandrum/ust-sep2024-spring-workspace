1f684c55b9d1d3b380defc3af23d4aed9312686e
bpoongs <bpoongs@gmail.com>
2024-09-26 17:01:44 +0530

    api-gateway

---
+++ api-gateway (2).zip_expanded/api-gateway/src/test/java/com/demo/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
